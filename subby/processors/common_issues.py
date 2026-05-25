48e1c538a46b092723cf42c84ee5cb8261e01979
gizeto <gizeto@proton.me>
2026-05-25 04:05:27 +0200

    Remove tags from punctuation-only spans

--- subby/processors/common_issues.py
+++ subby/processors/common_issues.py
@@ -128,6 +128,8 @@ class CommonIssuesFixer(BaseProcessor):
             line = re.sub(r'(?:</[a-z]>)(\s*)(?:<[a-z]>)', r'\1', line, flags=re.M)
             # Remove empty tags
             line = re.sub(r'<[a-z]>\s*</[a-z]>', r'', line)
+            # Remove tags from punctuation-only spans
+            line = re.sub(r'<[a-z]>([.,!?;:]+)</[a-z]>', r'\1', line)
             # Move "{\an8}" to the rest of the text if it's on a new line
             line = re.sub(r'({\\an8\})\n', r'\1', line)
             # Add closing italics tag if the line lacks one
